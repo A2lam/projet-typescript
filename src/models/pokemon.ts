f488ba45f445d1e4de3db506290bd5b5c19996fb
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2020-05-11 16:39:03 +0200

    Implemented attack method

--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -1,7 +1,7 @@
 import PokemonInterface from '../interfaces/pokemon';
 
 class Pokemon implements PokemonInterface {
-    constructor(public name: string, public speed: number) {}
+    constructor(public name: string, public life: number, public speed: number) {}
 
     attackFirst(p2: Pokemon): boolean {
         const attackFirst: boolean = this.speed >= p2.speed;
@@ -9,6 +9,10 @@ class Pokemon implements PokemonInterface {
 
         console.log(`${attackingFirst} attaque en premier !`);
         return attackFirst;
+    }
+
+    attack(p2: Pokemon): void {
+        p2.life -= 1;
     }
 }
 
--- tests/models/pokemon.test.ts
+++ tests/models/pokemon.test.ts
@@ -2,23 +2,33 @@ import Pokemon from '../../src/models/pokemon';
 
 describe('Pokemon First Attack', () => {
     it('should be P1 to attack first when P1 speed > P2 speed', function () {
-        let P1 = new Pokemon("P1", 5);
-        let P2 = new Pokemon("P2", 4);
+        let P1 = new Pokemon("P1", 3, 5);
+        let P2 = new Pokemon("P2", 3, 4);
 
         expect(P1.attackFirst(P2)).toBe(true);
     });
 
     it('should be P1 to attack first when P1 speed = P2 speed', function () {
-        let P1 = new Pokemon("P1", 5);
-        let P2 = new Pokemon("P2", 5);
+        let P1 = new Pokemon("P1", 3, 5);
+        let P2 = new Pokemon("P2", 3, 5);
 
         expect(P1.attackFirst(P2)).toBe(true);
     });
 
     it('should be P2 to attack first when P1 speed < P2 speed', function () {
-        let P1 = new Pokemon("P1", 4);
-        let P2 = new Pokemon("P2", 5);
+        let P1 = new Pokemon("P1", 3, 4);
+        let P2 = new Pokemon("P2", 3, 5);
 
         expect(P1.attackFirst(P2)).toBe(false);
+    });
+});
+
+describe('Pokemon Attack', () => {
+    it('should remain 2 in P2 life when P1 attack P2', function () {
+        let P1 = new Pokemon("P1", 3, 4);
+        let P2 = new Pokemon("P2", 3, 5);
+
+        P1.attack(P2);
+        expect(P2.life).toBe(2);
     });
 });
