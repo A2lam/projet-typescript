6ff739117b840c35a9b9ee1f2c90d189d433af8d
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2020-05-11 16:25:22 +0200

    Added 2 more tests to test suite Pokemon First Attack

--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -3,8 +3,12 @@ import PokemonInterface from '../interfaces/pokemon';
 class Pokemon implements PokemonInterface {
     constructor(public name: string, public speed: number) {}
 
-    attack(p2: Pokemon): string {
-        return (this.speed > p2.speed) ? this.name : p2.name;
+    attackFirst(p2: Pokemon): boolean {
+        const attackFirst: boolean = this.speed >= p2.speed;
+        const attackingFirst: string = (attackFirst) ? this.name : p2.name;
+
+        console.log(`${attackingFirst} attaque en premier !`);
+        return attackFirst;
     }
 }
 
--- tests/models/pokemon.test.ts
+++ tests/models/pokemon.test.ts
@@ -1,10 +1,24 @@
 import Pokemon from '../../src/models/pokemon';
 
-describe('Pokemon', () => {
-    it('should be P1 to attack first when add P1 speed > P2', function () {
+describe('Pokemon First Attack', () => {
+    it('should be P1 to attack first when P1 speed > P2 speed', function () {
         let P1 = new Pokemon("P1", 5);
         let P2 = new Pokemon("P2", 4);
 
-        expect(P1.attack(P2)).toBe("P1");
+        expect(P1.attackFirst(P2)).toBe(true);
+    });
+
+    it('should be P1 to attack first when P1 speed = P2 speed', function () {
+        let P1 = new Pokemon("P1", 5);
+        let P2 = new Pokemon("P2", 5);
+
+        expect(P1.attackFirst(P2)).toBe(true);
+    });
+
+    it('should be P2 to attack first when P1 speed < P2 speed', function () {
+        let P1 = new Pokemon("P1", 4);
+        let P2 = new Pokemon("P2", 5);
+
+        expect(P1.attackFirst(P2)).toBe(false);
     });
 });
